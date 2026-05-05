7dc16a65b1b3123b8d7f482c41898c813389d5a5
Илья Голубчиков <ilya.golubchikov.04@mail.ru>
2026-05-05 18:43:34 +0700

    Update main.rs

--- main.rs
+++ main.rs
@@ -3,3 +3,18 @@ pub extern "C" fn _start() {
     TEST::std::mkdir("my_dir");
     exit();
 }
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() {
+    let mut task = multitasking::Task::new(subproc(), Some(5));
+    syscall::spawn_task((&mut task as *mut multitasking::Task) as u64);
+    exit();
+}
+
+async fn subproc() {
+    for i in 0..15 {
+        println!("A");
+        sleep(2000).await;
+    }
+    exit();
+}
