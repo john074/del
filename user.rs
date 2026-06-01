365b7b0c6178761c850e4cf91a142d448eb14654
Илья Голубчиков <ilya.golubchikov.04@mail.ru>
2026-06-01 14:17:21 +0700

    Update user.rs

--- user.rs
+++ user.rs
@@ -8,12 +8,12 @@ use TEST::{ print, println };
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() {
-    let mut task = multitasking::Task::new(user(), Some(5));
+    let mut task = multitasking::Task::new(subproc(), Some(5));
     syscall::spawn_task((&mut task as *mut multitasking::Task) as u64);
     exit();
 }
 
-async fn user() {
+async fn subproc() {
     for i in 0..15 {
         println!("A");
         sleep(2000).await;
