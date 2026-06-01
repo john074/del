31cfb9f0c92d0ab66cf96bdca9e292fdc1f474dc
Илья Голубчиков <ilya.golubchikov.04@mail.ru>
2026-06-01 14:16:39 +0700

    Create user.rs

---
+++ user.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+use TEST::std::{ syscall, exit, sleep, multitasking };
+use TEST::{ print, println };
+
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() {
+    let mut task = multitasking::Task::new(user(), Some(5));
+    syscall::spawn_task((&mut task as *mut multitasking::Task) as u64);
+    exit();
+}
+
+async fn user() {
+    for i in 0..15 {
+        println!("A");
+        sleep(2000).await;
+    }
+    exit();
+}
